08331224baca56571bd21c4305902251dae78e98
Эмиль Абдуллаев <abdullaev_2000@mail.ru>
2022-12-06 16:10:00 +0300

    graphic_arts

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
-        return (f'{char_name} нанёс урон противнику равный'
+        return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
     if char_class == 'mage':
-        return (f'{char_name} нанёс урон противнику равный'
+        return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(5, 10)}')
     if char_class == 'healer':
-        return (f'{char_name} нанёс урон противнику равный'
+        return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(-3, -1)}')
     return (f'{char_name} не нанёс противнику урона')
 
@@ -26,7 +27,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 def special(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
-        return (f'{char_name} применил специальное умение «Выносливость'
+        return (f'{char_name} применил специальное умение «Выносливость '
                 f'{80 + 25}»')
     if char_class == 'mage':
         return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
@@ -83,6 +84,7 @@ def choice_char_class() -> str:
 
 
 def main() -> None:
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
